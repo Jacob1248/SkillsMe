8f92e00602ad86a87e23e9ff94bdcf05f453b9a5
Jacob1248 <jacobchacko228@gmail.com>
2020-10-05 17:55:37 +0530

    Finished judge page

---
+++ src/JudgePage/Judge.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import "./JudgePage.css";
+import gsap from 'gsap';
+
+export const Judge = (props) =>{
+    const imagesrc = `https://myskillsme.com/images/judges`;
+
+    let g = gsap.timeline();
+
+    const hoverMouse = (e) =>{
+        if(props.name){
+            let parent = e.target.parentNode;
+            let elem1 = parent.firstChild;
+            let elem2 = parent.lastChild;
+            g
+            .clear()
+            .to(elem1,{zIndex:"1001",duration:0},0)
+            .to(elem1,{display:"block",duration:0},0)
+            .to(elem2,{zIndex:"1002",duration:0},0)
+            .to(elem1,{height:"fit-content",opacity:1,duration:0.2},0.1)
+        }
+    }
+
+    const hoverMouseEnd = (e) =>{
+        if(props.name){
+            let parent = e.target.parentNode;
+            let elem1 = parent.firstChild;
+            let elem2 = parent.lastChild;
+            g
+            .clear()
+            .to(elem1,{height:"0rem",opacity:0,duration:0.2},0)
+            .to(elem1,{zIndex:"0",duration:0},0.1)
+            .to(elem1,{display:"none",duration:0},0.1)
+            .to(elem2,{zIndex:"1000",duration:0},0.1)
+        }
+    }
+
+    return(
+        <div className="judge-container">
+            <div className="modal-judge">
+                    <span>{props.name}</span>
+                    <span>{props.post}</span>
+            </div>
+            <img onMouseEnter={(e)=>hoverMouse(e)} onMouseLeave={(e)=>hoverMouseEnd(e)} src={`${imagesrc}/${props.src}`}></img>
+        </div>
+    )
+}
--- src/JudgePage/JudgePage.jsx
+++ src/JudgePage/JudgePage.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import "./JudgePage.css";
-
+import {Judge} from "./Judge"
 export const JudgePage = () =>{
 
     return(
         <div className="judge-page">
-            
+            <div className="explanation-container" style={{width:"100%"}}>
+                <span className="purple-header">60+ users with our judge badge</span>
+                <span className="explanation-header">Meet our judge users</span>
+                <div className="swirl">
+                </div><div className="logo-container">
+                    <div className="logo-row" style={{marginTop:"2rem"}}>
+                        <Judge name={'Kyle Martin'} src={`kyle_martin.jpg`}></Judge>
+                        <Judge name={'Aziz Ahmed'} src={`aziz_ahmed.jpg`}></Judge>
+                        <Judge name={'Richard Hpa'} src={`richard_hpa.jpg`}></Judge>
+                        <Judge name={'Grayson Orr'} src={`grayson_orr.jpg`}></Judge>
+                        <Judge name={'Rushabh Master'} src={`rushabh_master.jpg`}>></Judge>
+                        <Judge name={'Sherif Gayed'} src={`sherif_gayed.jpg`}></Judge>
+                        <Judge name={'Giles Thompson'} src={`giles_thompson.jpg`}></Judge>
+                        <Judge name={'Tony liu'} src={`tony_liu.jpg`}></Judge>
+                    </div>
+                    <div className="logo-row" style={{paddingLeft:'5rem',marginTop:"2rem"}}>
+                        <Judge name={'Sam Alhambra'} src={`sam_alhambra.jpg`}></Judge>
+                        <Judge name={'Darshit Pandya'} src={`darshit_pandya.jpg`}></Judge>
+                        <Judge name={'Charu Matta'} src={`charu_matta.jpg`}></Judge>
+                        <Judge name={'Walaa Elzayady'} src={`walaa_elzayady.jpg`}></Judge>
+                        <Judge name={'Tien Ngyuyen Khac'} src={`tien_nguyen_khac.jpg`}></Judge>
+                        <Judge name={'Akshay Raj'} src={`akshay_raj_gollahalli.jpg`}></Judge>
+                        <Judge name={'Feng Guo'} src={`feng_guo.png`}></Judge>
+                        <Judge name={'Ayush Narula'} src={`ayush_narula.jpg`}></Judge>
+                    </div>
+                    <div className="logo-row" style={{marginTop:"2rem"}}>
+                        <Judge name={'Divay Shrivastav'} src={`divay_shrivastav.jpg`}></Judge>
+                        <Judge name={'Daniel Munn'} src={`daniel_munn.jpg`}></Judge>
+                        <Judge name={'Cheng Zhen Yang'} src={`cheng_zhen_yang.png`}></Judge>
+                        <Judge name={'Hiruna Jayamanne'} src={`hiruna_jayamanne.jpg`}></Judge>
+                        <Judge name={'Philip Dolbel'} src={`philip_dolbel.png`}></Judge>
+                        <Judge name={'Ratha Long'} src={`ratha_long.jpg`}></Judge>
+                        <Judge name={'Rey Vincent'} src={`rey_vincent_babilonia.png`}></Judge>
+                        <Judge name={'Jianbin Chen'} src={`jianbin_chen.jpg`}></Judge>
+                    </div>
+                    <div className="logo-row" style={{marginTop:"2rem"}}>
+                        <Judge name={'Patrick Tan'} src={`patrick_tan.jpg`}></Judge>
+                        <Judge src={`more.jpg`}></Judge>
+                    </div>
+            </div>
+            </div>
         </div>
     )
 }
